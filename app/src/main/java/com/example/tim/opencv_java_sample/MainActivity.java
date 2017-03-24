88ce1d660a3d8e7db2f8a6076b3ef00a75bbc91d
Tim <fbfbfbtom@gmail.com>
2017-03-24 22:49:51 +0800

    OpenCV Sample

---
+++ app/src/main/java/com/example/tim/opencv_java_sample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tim.opencv_java_sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+}
